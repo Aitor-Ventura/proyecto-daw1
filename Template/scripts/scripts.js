e6bdc46f49287255b1cb65e11d70034d6dab62e8
Aitor <aitor.ventura00@gmail.com>
2021-11-15 21:48:48 +0000

    fix: renamed tailwind css to normal css in category

--- Template/scripts/scripts.js
+++ Template/scripts/scripts.js
@@ -105,10 +105,7 @@ function toggleWishlist(id) {
  * dark mode?
  */
 const checkbox = document.querySelector("#checkbox");
-/**
- * por la cara esto no lo pilla una vez abres la página por primera vez, pero lo añades tú por tu cuenta en consola y funciona perfect. wtf
- */
-const html = document.querySelector("html");
+const html = document.querySelector("#html");
 const toggleDarkMode = function() {
     checkbox.checked ? html.classList.add("dark") : html.classList.remove("dark");
 }
