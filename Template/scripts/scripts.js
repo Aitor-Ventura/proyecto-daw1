1f49a37e5d5a7a7de18bf33baa96ca0318f50229
AntonioMMartel <60984007+AntonioMMartel@users.noreply.github.com>
2021-11-23 22:24:26 +0000

    fixed remove item dark mode in cart.html

--- Template/scripts/scripts.js
+++ Template/scripts/scripts.js
@@ -526,7 +526,7 @@ const loadCartProducts = async function () {
                                     <line x1="18" y1="6" x2="6" y2="18" />
                                     <line x1="6" y1="6" x2="18" y2="18" />
                                     </svg>
-                                    <div class="ml-8"> Remove item </div>
+                                    <div class="ml-8 dark:text-black"> Remove item </div>
                                 </div>
                             </button>
                         </div>
